44f8e0ad1f7b1ba4a48ad4626f09aade8ba6f070
Chaeso99 <storymmm@naver.com>
2022-04-12 23:20:04 +0900

    04.12 23:19 체력감소
    몬스터와 데드존(하얀색 타일) 충돌시 플레이어 체력 감소

--- Slime Tower Defence/Assets/_Scripts/DeadZoneDetcet.cs
+++ Slime Tower Defence/Assets/_Scripts/DeadZoneDetcet.cs
@@ -4,16 +4,6 @@ using UnityEngine;
 
 public class DeadZoneDetcet : MonoBehaviour
 {
-    void Start()
-    {
-        
-    }
-
-    void Update()
-    {
-        
-    }
-
     void OnTriggerEnter(Collider other) //몬스터와 충돌 감지, 플레이어 체력 감소
     {
         Destroy(other.gameObject);
