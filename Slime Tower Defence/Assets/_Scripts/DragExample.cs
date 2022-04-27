544fb6aad19fb125eb3bc6aed085791795069e80
portalchu <56888404+portalchu@users.noreply.github.com>
2022-04-27 14:40:27 +0900

    0427 - 주석 추가

--- Slime Tower Defence/Assets/_Scripts/DragExample.cs
+++ Slime Tower Defence/Assets/_Scripts/DragExample.cs
@@ -11,17 +11,15 @@ public class DragExample : MonoBehaviour, IDropHandler, IDragHandler, IBeginDrag
     //public Image data;
     //public Sprite data2;
     //public DragContainer dragContainer;
-    public GameObject gameObject;
 
-    public bool isDragging = true;
+    public GameObject gameObject; // 드래그에 쓸 오브젝트
 
-    public static DragExample dragExample;
+    public bool isDragging = true; // 드래그 중인지 확인
     
 
     // Start is called before the first frame update
     void Start()
     {
-        dragExample = this;
 
         defultTransform = this.transform;
     }
@@ -43,12 +41,14 @@ public class DragExample : MonoBehaviour, IDropHandler, IDragHandler, IBeginDrag
 
     }
 
+    // 드래그 이후 드롭 이벤트 때 사용
     public void OnDrop(PointerEventData eventData)
     {
         Debug.Log("Drop");
         // throw new System.NotImplementedException();
     }
 
+    // 드래그 시작 시 사용
     public void OnBeginDrag(PointerEventData eventData)
     {
         Debug.Log("Drag Start");
@@ -71,10 +71,12 @@ public class DragExample : MonoBehaviour, IDropHandler, IDragHandler, IBeginDrag
         
     }
 
+    // 드래그 중 프레임 단위로 호출
     public void OnDrag(PointerEventData eventData)
     {
         // Debug.Log("Drag");
 
+        // 드래그 중일 때 옮기는 이미지를 마우스에 따라가도록 조정
         if (isDragging)
         {
             //dragContainer.transform.position = eventData.position;
@@ -85,18 +87,16 @@ public class DragExample : MonoBehaviour, IDropHandler, IDragHandler, IBeginDrag
         // test
         //RaycastHit hit;
 
-        Vector3 cameraPosition = Camera.main.transform.position;
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition); // 카메라에서 나가는 선
 
-        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit[] raycastHits = Physics.RaycastAll(ray); // 카메라에서 나가는 선에 부딪힌 오브젝트 모두 계산
 
-        RaycastHit[] raycastHits = Physics.RaycastAll(ray);
-
-        foreach (RaycastHit hit in raycastHits)
+        foreach (RaycastHit hit in raycastHits) // 부딪힌 물체들로 반복문 실행
         {
-            string objectName = hit.collider.gameObject.name;
+            string objectName = hit.collider.gameObject.name; // 테스트로 오브젝트 이름 출력
             //Debug.Log(objectName);
 
-            if (hit.collider.tag == "Tile")
+            if (hit.collider.tag == "Tile") // 부딪힌 물체가 타일인지 확인
             {
                 GameObject hitTile = hit.collider.gameObject;
                 Renderer renderer = hitTile.GetComponentInChildren<Renderer>();
@@ -126,6 +126,7 @@ public class DragExample : MonoBehaviour, IDropHandler, IDragHandler, IBeginDrag
         }
     }
 
+    // 드래그 끝날 때 실행
     public void OnEndDrag(PointerEventData eventData)
     {
         Debug.Log("Drag End");
