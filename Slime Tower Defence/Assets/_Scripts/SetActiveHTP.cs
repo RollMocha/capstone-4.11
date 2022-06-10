4ee9eb6aaac175a7fb2fda453caeb7868a9d164b
Chaeso99 <storymmm@naver.com>
2022-06-10 22:42:33 +0900

    06.10 밸런스패치, UI조정
    몬스터 및 슬라임, Bullet 스펙 조정
    게임씬 초기 배치 슬라임들 삭제
    웨이브 시간, 수치 조정
    타이틀 씬 설명_상위슬라임 조합식 추가
    게임오버 텍스트 색 조정

--- Slime Tower Defence/Assets/_Scripts/SetActiveHTP.cs
+++ Slime Tower Defence/Assets/_Scripts/SetActiveHTP.cs
@@ -7,6 +7,7 @@ public class SetActiveHTP : MonoBehaviour
 {
     public GameObject HowToPlayobj;
     public GameObject Titleobj;
+    public GameObject Describe2obj;
 
     public void ClickHTPButton()//HowToPlayButton 클릭시
     {
@@ -19,6 +20,13 @@ public class SetActiveHTP : MonoBehaviour
     {
         HowToPlayobj.SetActive(false);
         //HowToPlayDiscribe오브젝트 비활성화
+        Describe2obj.SetActive(true);
+    }
+
+    public void ClickDescribe2()//Describe2 클릭시
+    {
+        Describe2obj.SetActive(false);
+        //Describe2 비활성화
         Titleobj.SetActive(true);
     }
 }
